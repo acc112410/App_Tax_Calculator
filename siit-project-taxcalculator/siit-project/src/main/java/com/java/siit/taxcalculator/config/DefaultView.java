2c290b67bbad4144527cf6564fc71244108e8677
andreeaciutescu <a.ciutescu@yahoo.com>
2021-10-21 16:56:58 +0300

    App_Tax_Calculator

---
+++ siit-project-taxcalculator/siit-project/src/main/java/com/java/siit/taxcalculator/config/DefaultView.java
@@ -0,0 +1,9 @@
+package com.java.siit.taxcalculator.config;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
